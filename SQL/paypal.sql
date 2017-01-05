f332b1c1a6f7dc829e0989d5b4213ae9b5ec43dd
Jason Huang <JasonHuang768@gmail.com>
2017-01-05 21:45:02 +0800

    fix some information

--- SQL/paypal.sql
+++ SQL/paypal.sql
@@ -1,30 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1
--- http://www.phpmyadmin.net
---
--- 主機: localhost
--- 產生日期: 2016 年 12 月 15 日 16:21
--- 伺服器版本: 5.6.28
--- PHP 版本: 5.2.17
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- 資料庫: `amg_all-belle-shop`
---
-
--- --------------------------------------------------------
-
---
--- 表的結構 `dev_eng_paypal`
---
 
 CREATE TABLE IF NOT EXISTS `paypal` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -36,7 +9,3 @@ CREATE TABLE IF NOT EXISTS `paypal` (
 
 
 ALTER TABLE order add token text NOT NULL;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
